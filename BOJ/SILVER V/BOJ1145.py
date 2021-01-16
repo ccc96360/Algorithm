2519e409927b3412e53f94683fb830c388e43474
ccc96360 <ccc96360@naver.com>
2021-01-16 18:38:42 +0900

    20210116 적어도 대부분의 배수

---
+++ BOJ/SILVER V/BOJ1145.py
@@ -0,0 +1,15 @@
+#BOJ1145 적어도 대부분의 배수 20210116  
+def main():
+    li = list(map(int, input().split()))
+    li.sort()
+    s = li[2]
+    while True:
+        cnt = 0
+        for i in li:
+            if s%i == 0: cnt += 1
+        if cnt >= 3:
+            break
+        s += 1
+    print(s)
+if __name__ == '__main__':
+    main()
