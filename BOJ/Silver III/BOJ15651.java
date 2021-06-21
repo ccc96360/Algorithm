7e8e95d11fc255cfeee45b8b6f4ee45307f35745
ccc96360 <ccc96360@naver.com>
2021-06-21 15:04:33 +0900

    20210621 N 과 M (3)

---
+++ BOJ/Silver III/BOJ15651.java
@@ -0,0 +1,38 @@
+//BOJ15651 N과 M (3) 20210621
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static int stoi(String v){
+        return Integer.parseInt(v);
+    }
+
+    public static void dfs(int idx, int[] selected, int[] arr, int m, int n) throws IOException {
+        if(idx == m){
+            for(int v: selected){
+                bw.write(v + " ");
+            }
+            bw.write("\n");
+        }
+        else {
+            for (int i =0; i < n; i++) {
+                selected[idx] = arr[i];
+                dfs(idx + 1, selected, arr, m, n);
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        String[] input = br.readLine().split(" ");
+        int n = stoi(input[0]), m = stoi(input[1]);
+        int[] arr = new int[n];
+        for(int i = 1; i <= n; i++){
+            arr[i-1] = i;
+        }
+        dfs(0, new int[m], arr, m, n);
+        bw.flush();
+        bw.close();
+    }
+}
