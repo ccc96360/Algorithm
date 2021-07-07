63b285c833f6617ea0cd9d653c6f592277c5fe03
ccc96360 <ccc96360@naver.com>
2021-07-07 17:49:20 +0900

    20210707 최대 힙

---
+++ BOJ/Silver II/BOJ11279.java
@@ -0,0 +1,29 @@
+//BOJ11279 최대 힙 20210707
+import java.io.*;
+import java.util.*;
+
+import static java.lang.Math.*;
+
+public class Main {
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int n = Integer.parseInt(br.readLine());
+        for(int i = 0; i < n; i++){
+            int x = Integer.parseInt(br.readLine());
+            if(x==0){
+                Integer v = pq.poll();
+                if(v != null)
+                    bw.write(-v + "\n");
+                else bw.write(0 + "\n");
+            }
+            else{
+                pq.add(-x);
+            }
+        }
+        bw.flush();
+        bw.close();
+    }
+}
