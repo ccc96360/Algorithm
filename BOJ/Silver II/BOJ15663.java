c14be1e7fe7019f0dd015e38b934170540a345d5
ccc96360 <ccc96360@naver.com>
2021-07-09 18:00:52 +0900

    20210702, 20210709 N과 M (9)

--- BOJ/Silver II/BOJ15663.java
+++ BOJ/Silver II/BOJ15663.java
@@ -1,52 +1,44 @@
 //BOJ15663 N과 M (9) 20210702
 import java.io.*;
 import java.util.*;
-import java.util.stream.Collectors;
 
-public class BOJ15663 {
-    private static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    private static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+public class Main {
+    private static final BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    private static int n;
+    private static int k;
+    private static String[] arr;
 
     public static void main(String[] args) throws IOException {
         StringTokenizer stk = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(stk.nextToken());
-        int m = Integer.parseInt(stk.nextToken());
+        n = Integer.parseInt(stk.nextToken());
+        k = Integer.parseInt(stk.nextToken());
 
+        arr = br.readLine().split(" ");
+        Arrays.sort(arr, Comparator.comparingInt(Integer::parseInt));
 
-        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).sorted().toArray();
-
-        HashSet<String> set = new HashSet<>();
-        int[] a = {1,2};
-        int[] b = {1,2};
-        String as = intArrToString(a);
-        String bs = intArrToString(b);
-        dfs(n,m, 0, arr, new int[m], new HashSet<>(), new HashSet<>());
+        dfs(0, "", new HashSet<>(), new HashSet<>());
         bw.flush();
         bw.close();
     }
 
-    public static void dfs(int n, int m, int idx, int[] arr, int[] nums, HashSet<Integer> visited, HashSet<String> visited2){
-        if(idx == m){
-            String tmp = intArrToString(nums);
-            if(!visited2.contains(tmp)){
-                visited2.add(tmp);
-                System.out.println(tmp);
+    public static void dfs( int cnt, String result, Set<String> visited, Set<Integer> visitedIdx) throws IOException {
+        if(cnt == k){
+            if(!visited.contains(result)){
+                visited.add(result);
+                bw.write(result + "\n");
             }
+            return;
         }
-        else{
-            for(int i = 0; i < n; i++){
-                if(!visited.contains(i)){
-                    nums[idx] = arr[i];
-                    visited.add(i);
-                    dfs(n, m, idx+1, arr, nums, visited, visited2);
-                    visited.remove(i);
-                }
-
-            }
+        for(int i = 0; i < n; i++){
+            if(visitedIdx.contains(i)) continue;
+            String tmp = new String(result);
+            if(tmp.equals("")) tmp += arr[i];
+            else tmp += " " + arr[i];
+            visitedIdx.add(i);
+            dfs(cnt + 1, tmp, visited, visitedIdx);
+            visitedIdx.remove(i);
         }
-    }
-
-    public static String intArrToString(int[] a){
-        return Arrays.stream(a).mapToObj(Integer::toString).collect(Collectors.joining(" "));
     }
 }
